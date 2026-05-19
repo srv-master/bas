311940658f0a2dffb567192d249a80594ee601c8
Johan Kellén <johan.kellen@utb.linkoping.se>
2026-05-19 14:12:47 +0000

    node lts 22

--- 1-nodejs/1.1.js
+++ 1-nodejs/1.1.js
@@ -1,2 +1,8 @@
 // kör mig genom att gå till terminalen, skriva cd 1-nodejs och sedan "node 1.1"
 console.log("utskrift från konsolen");
+
+// eslint hittar fel
+let a = 1;
+const b = 3;
+b = 4;
+consol.log("feeel");
--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 module.exports = [
   {
-    files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
+    files: ["**/*.js", "*.js", "**/*.mjs", "**/*.cjs"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "commonjs",
-      // Vi hårdkodar in de viktigaste globala variablerna för Node/Express
-      // så slipper vi vara beroende av externa npm-paket i detta steg!
       globals: {
         console: "readonly",
         process: "readonly",
@@ -16,9 +14,19 @@ module.exports = [
       },
     },
     rules: {
-      "no-undef": "error", // Rödmarkera consoll.log och okända variabler
-      "no-const-assign": "error", // Rödmarkera x = 3 (överskrivning av const)
-      "no-unused-vars": "warn", // Gulmarkera variabler som glömts bort
+      // ==========================================
+      // STAGE 1: BASIC (Alltid aktiverade)
+      "no-undef": "error", // Röd linje om man stavar fel på variabler/funktioner (t.ex. consoll.log)
+      "no-const-assign": "error", // Röd linje om man försöker skriva över en const (t.ex. x = 3)
+
+      // ==========================================
+      // STAGE 2: STÄDNING OCH ORDNING (Gul varning)
+      "no-unused-vars": "warn", // Gul linje om man skapar en variabel men glömmer att använda den
+      "no-var": "error", // enbart använda moderna "let" och "const".
+      "no-empty": "warn", // Sätter röd linje om de glömt att skriva kod inuti en if-sats eller en funktion.
+      curly: "error", // Kräver måsvingar även för enraders if-satser så att du inte råkar skriva kod som körs utanför if-satsen.
+      "no-redeclare": "error", // Förbjud att man deklarerar samma variabelnamn två gånger i samma block
+      eqeqeq: "warn", // Tvinga strikt användning av === istället för ==
     },
   },
 ];
